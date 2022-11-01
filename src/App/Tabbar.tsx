f03b1e0e8db16dac5e769a7e8f7aff0a6aa7b61c
bwrs-1 <taisei.nagano@bwrs.page>
2022-11-01 11:57:52 +0900

    フッター部分 修正

--- src/App/Tabbar.tsx
+++ src/App/Tabbar.tsx
@@ -9,11 +9,11 @@ const Content = () => {
   return (
     <div className="tabbar">
       <ul>
-        <li><Link to="/"><div className="icon"><FaHome /></div><div className="text">ホーム</div></Link></li>
-        <li><Link to="/list"><div className="icon"><FaList /></div><div className="text">一覧</div></Link></li>
-        <li><Link to="/category"><div className="icon"><FaSearch /></div><div className="text">カテゴリ</div></Link></li>
-        <li><Link to="/images"><div className="icon"><FaCamera /></div><div className="text">写真から探す</div></Link></li>
-        <li><Link to="/about"><div className="icon"><AiOutlineAppstore /></div><div className="text">マップについて</div></Link></li>
+        <li><Link to="/"><div className="icon"><FaHome /></div><div className="text">Map</div></Link></li>
+        <li><Link to="/list"><div className="icon"><FaList /></div><div className="text">List</div></Link></li>
+        <li><Link to="/category"><div className="icon"><FaSearch /></div><div className="text">Category</div></Link></li>
+        <li><Link to="/images"><div className="icon"><FaCamera /></div><div className="text">Search Photo</div></Link></li>
+        <li><Link to="/about"><div className="icon"><AiOutlineAppstore /></div><div className="text">About</div></Link></li>
       </ul>
     </div>
   );
