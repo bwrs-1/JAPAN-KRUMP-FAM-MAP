1c8d88db962aee6d8a343204362d61f1fd7550fb
bwrs-1 <taisei.nagano@bwrs.page>
2022-11-11 15:52:48 +0900

    map表示 修正

--- src/App/Map.tsx
+++ src/App/Map.tsx
@@ -1,37 +1,36 @@
 import React from "react";
 // @ts-ignore
-import geojsonExtent from '@mapbox/geojson-extent'
-import toGeoJson from './toGeoJson'
-import setCluster from './setCluster'
-import Shop from './Shop'
+import geojsonExtent from "@mapbox/geojson-extent";
+import toGeoJson from "./toGeoJson";
+import setCluster from "./setCluster";
+import Shop from "./Shop";
 
 type Props = {
   data: Pwamap.ShopData[];
 };
 
 const CSS: React.CSSProperties = {
-  width: '100%',
-  height: '100%',
-  position: 'relative',
-}
+  width: "100%",
+  height: "100%",
+  position: "relative",
+};
 
 const hidePoiLayers = (map: any) => {
-
   const hideLayers = [
-    'poi',
-    'poi-primary',
-    'poi-r0-r9',
-    'poi-r10-r24',
-    'poi-r25',
-    'poi-bus',
-    'poi-entrance',
-  ]
+    "poi",
+    "poi-primary",
+    "poi-r0-r9",
+    "poi-r10-r24",
+    "poi-r25",
+    "poi-bus",
+    "poi-entrance",
+  ];
 
   for (let i = 0; i < hideLayers.length; i++) {
     const layerId = hideLayers[i];
-    map.setLayoutProperty(layerId, 'visibility', 'none')
+    map.setLayoutProperty(layerId, "visibility", "none");
   }
-}
+};
 
 const parseHash = (url?: Location | URL) => {
   const qstr = (url || window.location).hash.substring(2);
@@ -40,189 +39,210 @@ const parseHash = (url?: Location | URL) => {
 };
 
 const updateHash = (q: URLSearchParams) => {
-
   const hash = q.toString();
   if (hash) {
-    window.location.hash = `#/?${q.toString().replace(/%2F/g, '/')}`;
+    window.location.hash = `#/?${q.toString().replace(/%2F/g, "/")}`;
   }
 };
 
 const Content = (props: Props) => {
   const mapNode = React.useRef<HTMLDivElement>(null);
-  const [mapObject, setMapObject] = React.useState<any>()
-  const [shop, setShop] = React.useState<Pwamap.ShopData | undefined>(undefined)
-  const [ zLatLngString, setZLatLngString ] = React.useState<string>('');
+  const [mapObject, setMapObject] = React.useState<any>();
+  const [shop, setShop] = React.useState<Pwamap.ShopData | undefined>(
+    undefined
+  );
+  const [zLatLngString, setZLatLngString] = React.useState<string>("");
 
   const addMarkers = (mapObject: any, data: any) => {
-
     if (!mapObject || !data) {
-      return
+      return;
     }
 
-    mapObject.on('render', () => {
-
+    mapObject.on("render", () => {
       // nothing to do if shops exists.
-      if (mapObject.getSource('shops')) {
-        return
+      if (mapObject.getSource("shops")) {
+        return;
       }
 
-      hidePoiLayers(mapObject)
+      hidePoiLayers(mapObject);
 
-      const textColor = '#000000'
-      const textHaloColor = '#FFFFFF'
+      const textColor = "#000000";
+      const textHaloColor = "#FFFFFF";
 
-      const geojson = toGeoJson(data)
+      const geojson = toGeoJson(data);
 
-      mapObject.addSource('shops', {
-        type: 'geojson',
+      mapObject.addSource("shops", {
+        type: "geojson",
         data: geojson,
         cluster: true,
         clusterMaxZoom: 14,
         clusterRadius: 25,
-      })
+      });
 
       mapObject.addLayer({
-        id: 'shop-points',
-        type: 'circle',
-        source: 'shops',
-        filter: ['all',
-          ['==', '$type', 'Point'],
-        ],
+        id: "shop-points",
+        type: "circle",
+        source: "shops",
+        filter: ["all", ["==", "$type", "Point"]],
         paint: {
-          'circle-radius': 13,
-          'circle-color': '#FF0000',
-          'circle-opacity': 0.4,
-          'circle-stroke-width': 2,
-          'circle-stroke-color': '#FFFFFF',
-          'circle-stroke-opacity': 1,
+          "circle-radius": 13,
+          "circle-color": "#de9e09",
+          "circle-opacity": 0.4,
+          "circle-stroke-width": 2,
+          "circle-stroke-color": "#FFFFFF",
+          "circle-stroke-opacity": 1,
         },
-      })
+      });
 
       mapObject.addLayer({
-        id: 'shop-symbol',
-        type: 'symbol',
-        source: 'shops',
-        filter: ['all',
-          ['==', '$type', 'Point'],
-        ],
+        id: "shop-symbol",
+        type: "symbol",
+        source: "shops",
+        filter: ["all", ["==", "$type", "Point"]],
         paint: {
-          'text-color': textColor,
-          'text-halo-color': textHaloColor,
-          'text-halo-width': 2,
+          "text-color": textColor,
+          "text-halo-color": textHaloColor,
+          "text-halo-width": 2,
         },
         layout: {
-          'text-field': "{スポット名}",
-          'text-font': ['Noto Sans Regular'],
-          'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
-          'text-radial-offset': 0.5,
-          'text-justify': 'auto',
-          'text-size': 12,
-          'text-anchor': 'top',
-          'text-max-width': 12,
-          'text-allow-overlap': false,
+          "text-field": "{スポット名}",
+          "text-font": ["Noto Sans Regular"],
+          "text-variable-anchor": ["top", "bottom", "left", "right"],
+          "text-radial-offset": 0.5,
+          "text-justify": "auto",
+          "text-size": 12,
+          "text-anchor": "top",
+          "text-max-width": 12,
+          "text-allow-overlap": false,
         },
-      })
+      });
 
-      mapObject.on('mouseenter', 'shop-points', () => {
-        mapObject.getCanvas().style.cursor = 'pointer'
-      })
+      // 座標情報の分だけロゴを表示
+      // if (data !== void 0) {
+      //   for (let index = 0; index < data.length; index++) {
+      //     mapObject.loadImage(
+      //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRngdTKUc8b24IT3uIhN5BZV5uSBXjU5N5SfA&usqp=CAU",
+      //       function (error: any, res: string) {
+      //         mapObject.addImage("cat", res);
+      //       }
+      //     );
 
-      mapObject.on('mouseleave', 'shop-points', () => {
-        mapObject.getCanvas().style.cursor = ''
-      })
+      //     mapObject.addSource("point", {
+      //       type: "geojson",
+      //       data: geojson,
+      //       coordinates: [[data[index]["緯度"], data[index]["経度"]]],
+      //     });
 
-      mapObject.on('mouseenter', 'shop-symbol', () => {
-        mapObject.getCanvas().style.cursor = 'pointer'
-      })
+      //     mapObject.addLayer({
+      //       id: "logo",
+      //       type: "symbol",
+      //       source: "shops",
+      //       layout: {
+      //         "icon-image": "cat",
+      //         "icon-size": 0.2,
+      //       },
+      //     });
+      //   }
+      // }
 
-      mapObject.on('mouseleave', 'shop-symbol', () => {
-        mapObject.getCanvas().style.cursor = ''
-      })
+      mapObject.on("mouseenter", "shop-points", () => {
+        mapObject.getCanvas().style.cursor = "pointer";
+      });
 
-      mapObject.on('click', 'shop-points', (event: any) => {
+      mapObject.on("mouseleave", "shop-points", () => {
+        mapObject.getCanvas().style.cursor = "";
+      });
+
+      mapObject.on("mouseenter", "shop-symbol", () => {
+        mapObject.getCanvas().style.cursor = "pointer";
+      });
+
+      mapObject.on("mouseleave", "shop-symbol", () => {
+        mapObject.getCanvas().style.cursor = "";
+      });
+
+      // test
+      // mapObject.on("mouseenter", "logo", () => {
+      //   mapObject.getCanvas().style.cursor = "";
+      // });
+
+      mapObject.on("click", "shop-points", (event: any) => {
         if (!event.features[0].properties.cluster) {
-          setShop(event.features[0].properties)
+          setShop(event.features[0].properties);
         }
-      })
+      });
 
-      mapObject.on('click', 'shop-symbol', (event: any) => {
+      mapObject.on("click", "shop-symbol", (event: any) => {
         if (!event.features[0].properties.cluster) {
-          setShop(event.features[0].properties)
+          setShop(event.features[0].properties);
         }
-      })
+      });
 
-      setCluster(mapObject)
-
-
+      setCluster(mapObject);
     });
-
-  }
+  };
 
   React.useEffect(() => {
-
-    addMarkers(mapObject, props.data)
-
-  }, [mapObject, props.data])
+    addMarkers(mapObject, props.data);
+  }, [mapObject, props.data]);
 
   React.useEffect(() => {
     const hash = parseHash();
     if (zLatLngString) {
-      hash.set('map', zLatLngString);
+      hash.set("map", zLatLngString);
     }
     updateHash(hash);
-
-  }, [ zLatLngString ]);
+  }, [zLatLngString]);
 
   React.useEffect(() => {
     // Only once reder the map.
     if (!mapNode.current || mapObject) {
-      return
+      return;
     }
 
     // @ts-ignore
     const { geolonia } = window;
 
-    const geojson = toGeoJson(props.data)
-    const bounds = geojsonExtent(geojson)
+    const geojson = toGeoJson(props.data);
+    const bounds = geojsonExtent(geojson);
 
     const map = new geolonia.Map({
       container: mapNode.current,
       // style: 'geolonia/gsi',
       // style: 'https://api.maptiler.com/maps/52fe0485-5021-4bcb-9bb1-7ccecdbe11e4/style.json?key=pNZ57kRwS9dOIeGPTCct',
-      style: 'https://api.maptiler.com/maps/52fe0485-5021-4bcb-9bb1-7ccecdbe11e4/style.json?key=pNZ57kRwS9dOIeGPTCct',
+      style:
+        "https://api.maptiler.com/maps/52fe0485-5021-4bcb-9bb1-7ccecdbe11e4/style.json?key=pNZ57kRwS9dOIeGPTCct",
       bounds: bounds,
       fitBoundsOptions: { padding: 50 },
     });
 
     const hash = parseHash();
-    if (hash && hash.get('map')) {
+    if (hash && hash.get("map")) {
+      const latLngString = hash.get("map") || "";
+      const zlatlng = latLngString.split("/");
 
-      const latLngString = hash.get('map') || '';
-      const zlatlng = latLngString.split('/');
+      const zoom = zlatlng[0];
+      const lat = zlatlng[1];
+      const lng = zlatlng[2];
 
-      const zoom = zlatlng[0]
-      const lat = zlatlng[1]
-      const lng = zlatlng[2]
-
-      map.flyTo({center: [lng, lat], zoom});
-
+      map.flyTo({ center: [lng, lat], zoom });
     } else if (bounds) {
-
-      map.fitBounds(bounds, { padding: 50 })
-
+      map.fitBounds(bounds, { padding: 50 });
     }
 
     const onMapLoad = () => {
-      hidePoiLayers(map)
-      setMapObject(map)
+      hidePoiLayers(map);
+      setMapObject(map);
 
-      map.on('moveend', () => {
+      map.on("moveend", () => {
         // see: https://github.com/maplibre/maplibre-gl-js/blob/ba7bfbc846910c5ae848aaeebe4bde6833fc9cdc/src/ui/hash.js#L59
         const center = map.getCenter(),
           rawZoom = map.getZoom(),
           zoom = Math.round(rawZoom * 100) / 100,
           // derived from equation: 512px * 2^z / 360 / 10^d < 0.5px
-          precision = Math.ceil((zoom * Math.LN2 + Math.log(512 / 360 / 0.5)) / Math.LN10),
+          precision = Math.ceil(
+            (zoom * Math.LN2 + Math.log(512 / 360 / 0.5)) / Math.LN10
+          ),
           m = Math.pow(10, precision),
           lng = Math.round(center.lng * m) / m,
           lat = Math.round(center.lat * m) / m,
@@ -230,27 +250,30 @@ const Content = (props: Props) => {
 
         setZLatLngString(`${zStr}/${lat}/${lng}`);
       });
-    }
+    };
 
     const orienteationchangeHandler = () => {
-      map.resize()
-    }
+      map.resize();
+    };
 
     // attach
-    map.on('load', onMapLoad)
+    map.on("load", onMapLoad);
 
-    window.addEventListener('orientationchange', orienteationchangeHandler)
+    window.addEventListener("orientationchange", orienteationchangeHandler);
 
     return () => {
       // detach to prevent memory leak
-      window.removeEventListener('orientationchange', orienteationchangeHandler)
-      map.off('load', onMapLoad)
-    }
-  }, [mapNode, mapObject, props.data])
+      window.removeEventListener(
+        "orientationchange",
+        orienteationchangeHandler
+      );
+      map.off("load", onMapLoad);
+    };
+  }, [mapNode, mapObject, props.data]);
 
   const closeHandler = () => {
-    setShop(undefined)
-  }
+    setShop(undefined);
+  };
 
   return (
     <div style={CSS}>
@@ -261,11 +284,7 @@ const Content = (props: Props) => {
         data-marker="off"
         data-gesture-handling="off"
       ></div>
-      {shop ?
-        <Shop shop={shop} close={closeHandler} />
-        :
-        <></>
-      }
+      {shop ? <Shop shop={shop} close={closeHandler} /> : <></>}
     </div>
   );
 };
